149a2545d43edb58a51b75ca9d440648a718e11d
Jason <jbugallo@uci.edu>
2020-12-13 01:35:13 -0800

    refactored add tag

--- pages/api/notes/add-tag.ts
+++ pages/api/notes/add-tag.ts
@@ -1,32 +1,15 @@
-import { addTag } from '../../../controllers/notes';
-import connect from '../../../middleware/connect';
-import { handlerType } from '../../../middleware/connect';
-import CustomStatusError from '../../../utility/CustomStatusError';
+import nc from 'next-connect';
+import onError from 'utility/onError';
+import authenticate from 'middleware/authenticate';
+import { ExtendedRequest, nextConnectDB } from 'middleware/connect';
+import { NextApiResponse } from 'next';
+import { addTag } from 'controllers/notes';
 
-const addNewNoteTag: handlerType = async (req, res, connection, models) => {
-    const { method } = req;
+const handler = nc<ExtendedRequest, NextApiResponse>({ onError });
 
-    try {
-        switch (method) {
-            case 'POST':
-                console.log('in post toggle favorite');
-                return await addTag(req, res, models);
-            default:
-                throw new CustomStatusError('Invalid http method', 405);
-        }
-    } catch (error) {
-        console.log(error);
-        if (!error.status) error.status = 500;
-        res.status(error.status).json({ message: error.message });
-    } finally {
-        connection.close();
-    }
-};
+handler.use(nextConnectDB);
+handler.use(authenticate);
 
-export default connect(addNewNoteTag);
+handler.post(addTag);
 
-export const config = {
-    api: {
-        externalResolver: true
-    }
-};
+export default handler;
