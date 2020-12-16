7657123db6753444ba18327bfd2331335d7a0acd
Jason <jbugallo@uci.edu>
2020-12-16 07:53:05 -0800

    added scrolling if note out of view in notes menu

--- components/NotesMenu/NotesMenu.tsx
+++ components/NotesMenu/NotesMenu.tsx
@@ -10,18 +10,25 @@ import {
 import AddIcon from '@material-ui/icons/Add';
 import SearchIcon from '@material-ui/icons/Search';
 import { useDispatch, useSelector } from 'react-redux';
-import { useMutateAddNote } from '../../hooks/noteMutationHooks';
-import useLoader from '../../hooks/useLoader';
+import { useMutateAddNote } from 'hooks/noteMutationHooks';
+import useLoader from 'hooks/useLoader';
 import {
     selectEditor,
     setCurrent,
     setIsLocalSearchOpen,
     setSearchQuery,
     toggleEdit
-} from '../../store/slices/editorSlice';
+} from 'store/slices/editorSlice';
 import classes from './NotesMenu.module.scss';
 import SortOptions from './SortOptions';
-import mod from '../../utility/mod';
+import mod from 'utility/mod';
+import dynamic from 'next/dynamic';
+const ScrollIntoViewIfNeeded = dynamic(
+    () => import('react-scroll-into-view-if-needed'),
+    {
+        ssr: false
+    }
+);
 
 const NotesMenu = () => {
     const [mutateAddNote, { isLoading }] = useMutateAddNote();
@@ -113,28 +120,34 @@ const NotesMenu = () => {
             <List className={classes.list}>
                 {notes.length > 0 ? (
                     notes.map((note: Note) => (
-                        <ListItem
-                            button
+                        <ScrollIntoViewIfNeeded
                             key={note._id}
-                            selected={note._id === current}
-                            style={{
-                                display: 'flex'
-                            }}
-                            onClick={() => noteSelectionHandler(note._id)}
+                            active={note._id === current}
                         >
-                            <ListItemText
-                                primary={note.title}
-                                classes={{ primary: classes['menu-item-text'] }}
-                            />
-                            {note.isTemp && (
-                                <Chip
-                                    label="unsaved"
-                                    color="primary"
-                                    size="small"
-                                    variant="outlined"
+                            <ListItem
+                                button
+                                selected={note._id === current}
+                                style={{
+                                    display: 'flex'
+                                }}
+                                onClick={() => noteSelectionHandler(note._id)}
+                            >
+                                <ListItemText
+                                    primary={note.title}
+                                    classes={{
+                                        primary: classes['menu-item-text']
+                                    }}
                                 />
-                            )}
-                        </ListItem>
+                                {note.isTemp && (
+                                    <Chip
+                                        label="unsaved"
+                                        color="primary"
+                                        size="small"
+                                        variant="outlined"
+                                    />
+                                )}
+                            </ListItem>
+                        </ScrollIntoViewIfNeeded>
                     ))
                 ) : (
                     <ListItemText
