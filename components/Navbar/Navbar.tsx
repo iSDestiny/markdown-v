9f306ad65bf31bd31aa9eabf00a3679df16551d3
Jason <jbugallo@uci.edu>
2020-12-21 21:48:03 -0800

    mobile nav icon

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -7,9 +7,16 @@ import { Button, Container, IconButton } from '@material-ui/core';
 import { useQuery } from 'react-query';
 import { fetchAuthInfo } from 'utility/fetchAuth';
 import Skeleton from '@material-ui/lab/Skeleton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useState } from 'react';
 
 const Navbar = () => {
     const router = useRouter();
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.down('xs'));
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     const { isLoading, isError, isSuccess } = useQuery(
         'authInfo',
         fetchAuthInfo,
@@ -32,84 +39,98 @@ const Navbar = () => {
                         />
                         <span>MarkdownV</span>
                     </a>
-                    <ul className={classes.content}>
-                        <li>
-                            <a href="#features">Features</a>
-                        </li>
-                    </ul>
-                </div>
-                <ul className={classes.right}>
-                    {isLoading ? (
-                        <>
+                    {!matches && (
+                        <ul className={classes.content}>
                             <li>
-                                <Skeleton
-                                    variant="text"
-                                    width={70}
-                                    height={45}
-                                    animation="wave"
-                                />
+                                <a href="#features">Features</a>
                             </li>
-                            <li>
-                                <Skeleton
-                                    variant="text"
-                                    width={70}
-                                    height={45}
-                                    animation="wave"
-                                />
-                            </li>
-                        </>
-                    ) : (
-                        <>
-                            {isError && (
-                                <>
-                                    <li style={{ marginRight: 5 }}>
-                                        <IconButton
-                                            onClick={() =>
-                                                window.open(
-                                                    'https://github.com/iSDestiny/markdown-v'
-                                                )
-                                            }
-                                            size="medium"
-                                        >
-                                            <GitHubIcon />
-                                        </IconButton>
-                                    </li>
-                                    <li>
-                                        <Link href="/login">Login</Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/signup">Sign up</Link>
-                                    </li>
-                                </>
-                            )}
-                            {isSuccess && (
-                                <>
-                                    <li style={{ marginRight: 0 }}>
-                                        <IconButton
-                                            onClick={() =>
-                                                window.open(
-                                                    'https://github.com/iSDestiny/markdown-v'
-                                                )
-                                            }
-                                            size="medium"
-                                        >
-                                            <GitHubIcon />
-                                        </IconButton>
-                                    </li>
-                                    <li>
-                                        <Button
-                                            variant="outlined"
-                                            color="primary"
-                                            onClick={() => router.push('/app')}
-                                        >
-                                            Launch App
-                                        </Button>
-                                    </li>
-                                </>
-                            )}
-                        </>
+                        </ul>
                     )}
-                </ul>
+                </div>
+                {matches && (
+                    <IconButton
+                        size="medium"
+                        onClick={() => setIsMenuOpen(true)}
+                    >
+                        <MenuIcon fontSize="inherit" />
+                    </IconButton>
+                )}
+                {!matches && (
+                    <ul className={classes.right}>
+                        {isLoading ? (
+                            <>
+                                <li>
+                                    <Skeleton
+                                        variant="text"
+                                        width={70}
+                                        height={45}
+                                        animation="wave"
+                                    />
+                                </li>
+                                <li>
+                                    <Skeleton
+                                        variant="text"
+                                        width={70}
+                                        height={45}
+                                        animation="wave"
+                                    />
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                {isError && (
+                                    <>
+                                        <li style={{ marginRight: 5 }}>
+                                            <IconButton
+                                                onClick={() =>
+                                                    window.open(
+                                                        'https://github.com/iSDestiny/markdown-v'
+                                                    )
+                                                }
+                                                size="medium"
+                                            >
+                                                <GitHubIcon />
+                                            </IconButton>
+                                        </li>
+                                        <li>
+                                            <Link href="/login">Login</Link>
+                                        </li>
+                                        <li>
+                                            <Link href="/signup">Sign up</Link>
+                                        </li>
+                                    </>
+                                )}
+                                {isSuccess && (
+                                    <>
+                                        <li style={{ marginRight: 0 }}>
+                                            <IconButton
+                                                onClick={() =>
+                                                    window.open(
+                                                        'https://github.com/iSDestiny/markdown-v'
+                                                    )
+                                                }
+                                                size="medium"
+                                            >
+                                                <GitHubIcon />
+                                            </IconButton>
+                                        </li>
+                                        <li>
+                                            <Button
+                                                variant="outlined"
+                                                color="primary"
+                                                onClick={() =>
+                                                    router.push('/app')
+                                                }
+                                            >
+                                                Launch App
+                                            </Button>
+                                        </li>
+                                    </>
+                                )}
+                            </>
+                        )}
+                    </ul>
+                )}
             </nav>
         </Container>
     );
